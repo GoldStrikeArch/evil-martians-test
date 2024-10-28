ea8d592d79bd673b4be611c143ed38f1b3fbe111
GoldStrikeArch <arthasmontano@mail.ru>
2024-10-28 13:00:15 +0100

    fix: github actions pipeline correct steps

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [pluginSvelte()],
   output: {
     injectStyles: true,
+    assetPrefix: 'https://goldstrikearch.github.io/evil-martians-test/'
   },
   html: {
     template: "./public/index.html",
